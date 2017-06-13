fc2063e3a683c05f0a17931eddb67dd1c8dda078
mcclanelee <mcclane@126.com>
2017-06-13 22:24:15 +0800

    changed

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
